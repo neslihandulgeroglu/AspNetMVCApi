e3c1bf54bc0e8f8f7ec7db7cf9f3305790dd964e
Neslihan Dülgeroğlu <neslihanndulgeroglu@gmail.com>
2022-05-18 10:29:33 +0300

    Add project files.

---
+++ AspNetMVCApi_PL/Controllers/StudentController.cs
@@ -0,0 +1,55 @@
+﻿using AspNetMVCApi_BLL.ContractBLL;
+using AspNetMVCApi_BLL.ImplementationBLL;
+using AspNetMVCApi_DAL;
+using AspNetMVCApi_DAL.Contracts;
+using AspNetMVCApi_DAL.Implementations;
+using AspNetMVCApi_EL.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace AspNetMVCApi_PL.Controllers
+{
+    [System.Web.Http.RoutePrefix("s")]
+    public class StudentController : ApiController
+    {
+        private readonly IStudentService _studentService;
+
+        public StudentController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        // GET api/<controller>
+        public ResponseData GetAllStudents()
+        {
+            try
+            {
+                var result = _studentService.GetAllStudents().Data;
+                return new ResponseData() {IsSuccess=true, Data=result};
+            } 
+            catch (Exception ex)
+            {
+                // ex loglanabilir
+                return new ResponseData()
+                {
+                    IsSuccess=false,
+                    Message=ex.Message
+                };
+            }
+        }
+        
+
+        // GET api/<controller>/5
+
+        // POST api/<controller>
+
+        // PUT api/<controller>/5
+
+        // DELETE api/<controller>/5
+    }
+}
